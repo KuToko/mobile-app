6f3d5016774dfa7c05d2bd8ff85323b1f0bd7028
jiddan300 <jiddan300@gmail.com>
2023-06-02 01:01:08 +0700

    UI beranda, Cari, dan Profile

--- app/src/main/java/com/example/kutoko/ui/beranda/BerandaFragment.kt
+++ app/src/main/java/com/example/kutoko/ui/beranda/BerandaFragment.kt
@@ -1,4 +1,4 @@
-package com.example.kutoko.ui.home
+package com.example.kutoko.ui.beranda
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.kutoko.databinding.FragmentHomeBinding
+import com.example.kutoko.databinding.FragmentBerandaBinding
 
-class HomeFragment : Fragment() {
+class BerandaFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentBerandaBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val berandaViewModel =
+            ViewModelProvider(this).get(BerandaViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentBerandaBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
