99a127c1744d4710b41fcc11adbc3fb7c29e9cb6
Jiddan <85534110+jiddan300@users.noreply.github.com>
2023-06-02 19:29:50 +0700

    Bottom nav (#1)
    * More BottomNav
    
    * UI beranda, Cari, dan Profile

--- app/src/main/java/com/example/kutoko/ui/beranda/BerandaViewModel.kt
+++ app/src/main/java/com/example/kutoko/ui/beranda/BerandaViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.kutoko.ui.home
+package com.example.kutoko.ui.beranda
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class BerandaViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
