855ac80f2dda53a07d8e0f197684c4c233edab36
Dedi <dedi.yanto@ti.ukdw.ac.id>
2023-06-13 07:45:13 +0700

    Update ProfileFragment.kt

--- app/src/main/java/com/example/kutoko/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/kutoko/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.kutoko.data.UserPreference
@@ -36,11 +37,19 @@ class ProfileFragment : Fragment() {
         mUserPreference = UserPreference(requireContext())
 
         binding.logout.setOnClickListener{
-            mUserPreference.deleteData()
-            TokenManager.token = ""
-            val moveIntent = Intent(requireContext(), LoginActivity::class.java)
-            startActivity(moveIntent)
-            requireActivity().finish()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Logout")
+            builder.setMessage("Are you sure you want to logout?")
+            builder.setPositiveButton("OK") { _, _ ->
+                mUserPreference.deleteData()
+                TokenManager.token = ""
+                val moveIntent = Intent(requireContext(), LoginActivity::class.java)
+                startActivity(moveIntent)
+                requireActivity().finishAffinity() // Close all activities
+            }
+            builder.setNegativeButton("Cancel", null)
+            val dialog = builder.create()
+            dialog.show()
         }
 
 
