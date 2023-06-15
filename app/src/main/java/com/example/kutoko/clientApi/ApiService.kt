c4b46f63ab2b9194374e61674fbddcbc23489abc
Dedi <dedi.yanto@ti.ukdw.ac.id>
2023-06-15 16:41:44 +0700

    update

--- app/src/main/java/com/example/kutoko/clientApi/ApiService.kt
+++ app/src/main/java/com/example/kutoko/clientApi/ApiService.kt
@@ -2,6 +2,8 @@ package com.example.kutoko.clientApi
 
 import com.example.kutoko.data.*
 import com.example.kutoko.data.apiResponse.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -63,4 +65,22 @@ interface ApiService {
         @Query("latitude") latitude: Double,
         @Query("longitude") longitude: Double,
     ): Call<FindBusinessResponse>
+
+    @Multipart
+    @POST("products")
+    fun uploadProduct(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Part("name") name : String,
+        @Part("price") price : Int,
+        @Part("business_id") business_id : String
+    ) : Call<UploadProductResponse>
+
+
+    @GET("businesses/my/business")
+    fun getMyStore(
+        @Header("Authorization") token: String?
+    ) : Call<MyStoreResponse>
+
 }
